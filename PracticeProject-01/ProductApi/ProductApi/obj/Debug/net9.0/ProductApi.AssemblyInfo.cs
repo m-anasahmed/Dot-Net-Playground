1b3c0978877c908f8f599288da2ec549059d9b80
Muhammad Anas <anasahmedofficial786@gmail.com>
2025-08-17 08:18:58 +0500

    Update ProductApi.AssemblyInfo.cs

--- PracticeProject-01/ProductApi/ProductApi/obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
+++ PracticeProject-01/ProductApi/ProductApi/obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8fb340ce0f1f2d49e6d75134ce213d9060e3b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
